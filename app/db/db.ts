8d1c0f2c0c3b609145487dde9b604162f24ca234
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-08-04 02:11:41 +0530

    added db

---
+++ app/db/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma: ReturnType<typeof prismaClientSingleton> = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV === 'development') globalThis.prismaGlobal = prisma
