2275355d906dd8c898fe58c4d4af207982986f58
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-08-04 18:17:04 +0530

    added hooks

---
+++ app/api/hooks/useTokens.ts
@@ -0,0 +1,28 @@
+import { TokenDetails } from "@/app/lib/tokens";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export interface TokenWithbalance extends TokenDetails {
+    balance: string;
+    usdBalance: string;
+}
+
+export function useTokens(address: string) {
+    const [tokenBalances, setTokenBalances] = useState<{
+        totalBalance: number,
+        tokens: TokenWithbalance[]
+    } | null >(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/api/tokens?address=${address}`)
+            .then(res => {
+                setTokenBalances(res.data);
+                setLoading(false)
+            })
+    }, [address])
+
+    return {
+        loading, tokenBalances
+    }
+}
