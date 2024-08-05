0e48f39f62138d53009ff65a47bd606a50a0455a
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-08-05 22:48:38 +0530

    addeed new component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -26,3 +26,12 @@ export const SecondaryButton = ({subject,onClick,prefix}:ButtonProps) => {
         </div>
  </button>
 }
+
+
+export const TabButton = ({active,children,onClick}:{
+    active:boolean;
+    children:React.ReactNode;
+    onClick: ()=> void
+}) => {
+    return <button type="button" className = {`text-white  ${active ? "bg-blue-700" : "bg-blue-400"} hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`} onClick={onClick} >{children}</button>
+}
